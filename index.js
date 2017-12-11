7a77233efedcdf66583e731fd444652b3a0c7123
Eric Lau <elau369@outlook.com>
2017-12-11 05:32:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,19 +20,15 @@ function logWhisper(string)
 
 
 
-function sayHiToGrandma(string)
-{
-  if (string === "I love you, Grandma.")
-    {
-      return "I love you, too." 
-    }
-  else if (string.toLowerCase() === true)
-    {
-      return "I can\'t hear you!"
-    }
-  else if (string.toUpperCase() === true)
-  {
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
     return "YES INDEED!"
-  }  
-}
-  
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }}
