67c7db864f4355adab0d90b57a9e2ee364acce4c
Eric Lau <elau369@outlook.com>
2017-12-11 05:26:45 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,38 @@
+function shout(a)
+{
+  return a.toUpperCase();
+}
+
+function whisper(b)
+{
+  return b.toLowerCase();
+}
+
+function logShout(c)
+{
+  console.log(c.toUpperCase());
+}
+
+function logWhisper(string)
+{
+  console.log(string.toLowerCase());
+}
+
+
+
+function sayHiToGrandma(string)
+{
+  if (string === "I love you, Grandma.")
+    {
+      return "I love you, too." 
+    }
+  else if (string.toLowerCase() === true)
+    {
+      return "I can\'t hear you!"
+    }
+  else if (string.toUpperCase() === true)
+  {
+    return "YES INDEED!"
+  }  
+}
+  
